342fd05ead20661be508577d2248b835d773fe6e
Wei Jing <weilking@gmail.com>
2025-09-24 23:26:19 +0200

    handle double C and T report of X, Y in  windows digitizer

--- hid_parser/__init__.py
+++ hid_parser/__init__.py
@@ -784,12 +784,34 @@ class ReportDescriptor:
         else:
             if len(usages) != report_count:
                 if len(usages) > report_count:
-                    error_str = f'Expecting {report_count} usages but got {len(usages)}'
-                    warnings.warn(HIDComplianceWarning(error_str), stacklevel=2)
-                    report_count = len(usages)
+                    warnings.warn(
+                        HIDComplianceWarning(
+                            f'Report count ({report_count}) is less than number of usages ({len(usages)}), \
+                                truncating usages.'
+                            )
+                        , stacklevel=2
+                    )
+                    usages = usages[:report_count]
                 else:
-                    for i in range(report_count - len(usages)):
-                        usages.append(usages[0])
+                    x_usage = Usage(
+                        hid_parser.data.UsagePages.GENERIC_DESKTOP_CONTROLS_PAGE, 
+                        hid_parser.data.GenericDesktopControls.X
+                        )
+                    y_usage = Usage(
+                        hid_parser.data.UsagePages.GENERIC_DESKTOP_CONTROLS_PAGE, 
+                        hid_parser.data.GenericDesktopControls.Y
+                        )
+                    if report_count > len(usages) and usages and usages[0] in (x_usage, y_usage):
+                        pad_usage = usages[0]
+                        for _ in range(report_count - len(usages)):
+                            usages.append(pad_usage)
+                    else:
+                        missing_usage_count = report_count - len(usages)
+                        usages += [] * missing_usage_count
+                        warning_message = f'Report count ({report_count}) is greater than number of \
+                            usages ({len(usages)}), share the unused usage count to next usage'
+                        warnings.warn(HIDComplianceWarning(warning_message), stacklevel=2)
+                    
 
             for usage in usages:
                 item = VariableItem(
